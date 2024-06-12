6be8ea052b8c5949aa1cb93ebd23b6d2335b53c9
jzpacheco <jeffersonpacheco@hotmail.com>
2024-06-12 19:12:05 -0300

    git init

---
+++ src/main/java/com/backend/projetofinal/GerenciadorDeConsultasMedicasApplication.java
@@ -0,0 +1,13 @@
+package com.backend.projetofinal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GerenciadorDeConsultasMedicasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GerenciadorDeConsultasMedicasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/backend/projetofinal/GerenciadorDeConsultasMedicasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.backend.projetofinal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GerenciadorDeConsultasMedicasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
